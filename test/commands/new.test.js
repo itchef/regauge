ecd22d6b8e7188817f2108ea1f17d8ceec698847
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-05-24 00:30:15 +0530

    Kaustav | #22 | Skipped test | TG-22

--- test/commands/new.test.js
+++ test/commands/new.test.js
@@ -7,11 +7,4 @@ describe('new', () => {
   .it('runs hello', ctx => {
     expect(ctx.stdout).to.contain('hello world')
   })
-
-  test
-  .stdout()
-  .command(['new', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
 })
