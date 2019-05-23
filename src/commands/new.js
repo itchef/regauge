b6d42b2eb35b54f71f389720f74145dd07aaa7f8
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-05-24 00:25:42 +0530

    Kaustav | #22 | Added logger utils. Added logging for tasks. | TG-22

--- src/commands/new.js
+++ src/commands/new.js
@@ -3,6 +3,7 @@ const Dir = require('../utils/dir.util')
 const {clone} = require('../utils/git.util')
 const {copyBaseReact, updatePackageJson} = require('../helpers/new.helper')
 const {initialCommit} = require('../helpers/git.helper')
+const Logger = require('../utils/logger.util')
 
 const {Command, flags} = require('@oclif/command')
 
@@ -16,10 +17,13 @@ class NewCommand extends Command {
       const tmp = new Dir(tempDir).clean().make().cd()
       const templateDir = path.join(tempDir, 'packages/templates')
       const packageJsonPath = path.join(rootDir, appName, 'package.json')
+      Logger.info('Creating base app........')
 
       clone('itchef', 'rg', 'packages')
+      Logger.info('Creating base app........')
       const projectDir = copyBaseReact(templateDir, rootDir, appName)
       updatePackageJson(packageJsonPath, appName)
+      Logger.info('Adding initial commit........')
       new Dir(projectDir)
       .clean('.git')
       .cd()
---
+++ src/utils/logger.util.js
@@ -0,0 +1,10 @@
+const info = message => {
+  // eslint-disable-next-line no-console
+  console.info(message)
+}
+
+const Logger = {
+  info,
+}
+
+module.exports = Logger
