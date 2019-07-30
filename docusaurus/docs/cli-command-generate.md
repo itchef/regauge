f4394b13de269f01ff0f135c1ca9a2e8aff4b2f9
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-07-30 15:23:16 +0530

    Kaustav | #29 | Added documentations for cli commands. | TG-29

---
+++ docusaurus/docs/cli-command-generate.md
@@ -0,0 +1,44 @@
+---
+id: cli-command-generate
+title: rg generate
+---
+
+Generates and/or modifies files based on a task.
+
+rg **generate** `<task>` `<name>`
+
+rg **g** `<task>` `<name>`
+
+## Description
+
+It generates the required element based on the task name.
+
+> Note that, for now we are only supporting **component** generating task.
+
+## Arguments
+
+Argument | Description |
+---------|----------|
+ _task_ | The task or collection of tasks to generate. Check the [currently supported tasks](#task-commands). |
+
+---
+
+## Task commands
+
+### Component
+
+rg generate **component** `<name>`
+
+rg g **component** `<name>`
+
+#### Description
+
+Creates a new generic component definition in the given or default project.
+
+The component will be created in the current working directory.
+
+#### Arguments
+
+Argument | Description |
+---------|----------|
+ _name_ | The name of the component. |
--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -1,33 +1,46 @@
 const siteConfig = {
-  title: 'rg-cli',
-  tagline: 'A command line interface for react app with webpack',
-  url: 'https://itchef.github.io',
-  baseUrl: '/rg-cli/',
-  projectName: 'rg-cli',
-  organizationName: 'itchef',
+  title: "rg-cli",
+  tagline: "A command line interface for react app with webpack",
+  url: "https://itchef.github.io",
+  baseUrl: "/rg-cli/",
+  projectName: "rg-cli",
+  organizationName: "itchef",
   headerLinks: [
-    {doc: 'introduction', label: 'DOCUMENTATION'},
-    {href: 'https://github.com/itchef/rg-cli', label: 'GITHUB'},
+    { doc: "introduction", label: "DOCUMENTATION" },
+    { href: "https://github.com/itchef/rg-cli", label: "GITHUB" }
   ],
 
-  headerIcon: 'img/Logo-rg-light.png',
-  footerIcon: 'img/Logo-rg-light.png',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/Logo-rg-light.png",
+  footerIcon: "img/Logo-rg-light.png",
+  favicon: "img/favicon.ico",
 
   colors: {
-    primaryColor: '#1A237E',
-    secondaryColor: '#3F51B5',
+    primaryColor: "#1A237E",
+    secondaryColor: "#3F51B5"
   },
 
-  copyright: `Copyright © ${new Date().getFullYear()} ITChef. Authored by Kaustav Chakraborty`,
-
+  copyright: {
+    prefix: `Copyright © ${new Date().getFullYear()}`,
+    owner: {
+      name: "ITChef",
+      link: "https://github.com/itchef"
+    }
+  },
+  license: {
+    name: "MIT-styled",
+    link: "https://github.com/itchef/rg-cli/blob/master/LICENSE"
+  },
+  author: {
+    name: "Kaustav Chakraborty",
+    link: "https://github.com/phoenixTW"
+  },
   highlight: {
-    theme: 'default',
+    theme: "default"
   },
 
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js"],
 
-  onPageNav: 'separate',
+  onPageNav: "separate",
   cleanUrl: true,
 
   enableUpdateTime: true,
@@ -36,11 +49,17 @@ const siteConfig = {
   homePage: {
     commands: [
       {
-        title: 'rg new',
-        description: 'The React Generator CLI makes it easy to create a react application with webpack that already works, right out of the box. It already follows react’s best practices!',
+        title: "rg new",
+        description:
+          "The React Generator CLI makes it easy to create a react application with webpack that already works, right out of the box. It already follows react’s best practices!"
       },
-    ],
-  },
-}
+      {
+        title: "rg generate",
+        description:
+          "Generates components with a simple command. The CLI also creates simple test file with it."
+      }
+    ]
+  }
+};
 
-module.exports = siteConfig
+module.exports = siteConfig;
