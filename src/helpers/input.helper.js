820d1cab240454f9c143d9dfe320ade3d60055ff
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-07-30 15:53:40 +0530

    Kaustav | #39 | Added prompts for description, version, license and keywords. | TG-39

--- src/helpers/input.helper.js
+++ src/helpers/input.helper.js
@@ -1,14 +1,30 @@
-const {cli} = require('cli-ux')
-const {getAuthorName} = require('./git.helper')
+const { cli } = require("cli-ux");
+const { getAuthorName } = require("./git.helper");
 
-const AUTHOR_NAME_KEY = 'Author Name'
+const AUTHOR_NAME_KEY = "Author Name";
+const DESCRIPTION_KEY = "Description";
+const VERSION_KEY = "Version";
+const LICENSE_KEY = "License";
+const KEYWORDS_KEY = "Keywords";
+
+const trimKeyword = keyword => keyword.trim();
 
 const processUserInput = async appName => {
-  const author = await cli.prompt(AUTHOR_NAME_KEY, {default: getAuthorName()})
+  const description = await cli.prompt(DESCRIPTION_KEY, { default: "" });
+  const author = await cli.prompt(AUTHOR_NAME_KEY, {
+    default: getAuthorName()
+  });
+  const version = await cli.prompt(VERSION_KEY, { default: "0.0.0" });
+  const license = await cli.prompt(LICENSE_KEY, { default: "ISC" });
+  const keywords = await cli.prompt(KEYWORDS_KEY, { default: "" });
   return {
     name: appName,
+    description,
     author,
-  }
-}
+    version,
+    license,
+    keywords: keywords.split(",").map(trimKeyword)
+  };
+};
 
-module.exports = {processUserInput}
+module.exports = { processUserInput };
