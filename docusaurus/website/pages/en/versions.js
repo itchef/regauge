6b8f4e4b4a02af7172c53369d0529e807631debc
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-08-07 10:33:05 +0530

    Kaustav | #43 | Added section for past versions. | TG-43

--- docusaurus/website/pages/en/versions.js
+++ docusaurus/website/pages/en/versions.js
@@ -35,6 +35,37 @@ const Version = ({ config, latest, repo, children }) => (
   </div>
 );
 
+const PastVersions = ({ children, latest, config }) => (
+  <div className="current-version__wrapper">
+    <h3 className="current-version__header">{children}</h3>
+    <p>Here you can find documentation for previous versions of regauge.</p>
+    <table className="versions__container">
+      <tbody>
+        {versions.map(
+          version =>
+            version !== latest && (
+              <tr key={version}>
+                <th>
+                  {version === versions[versions.length - 1] ? "<=" : ""}
+                  {version}
+                </th>
+                <td>
+                  <a
+                    href={`${config.baseUrl}${
+                      config.docsUrl
+                    }/${version}/installation`}
+                  >
+                    Documentation
+                  </a>
+                </td>
+              </tr>
+            )
+        )}
+      </tbody>
+    </table>
+  </div>
+);
+
 const Master = ({ config, repo, branch, children }) => (
   <div className="current-version__wrapper">
     <h3 className="current-version__header">{children}</h3>
@@ -71,6 +102,9 @@ const Versions = props => {
       <Master config={siteConfig} repo={repoUrl} branch="master">
         Latest Version <i>(Unreleased)</i>
       </Master>
+      <PastVersions config={siteConfig} latest={latestVersion}>
+        Past versions
+      </PastVersions>
       <p>
         You can find past versions of this project on{" "}
         <a href={`${repoUrl}/releases`}>GitHub</a>.
