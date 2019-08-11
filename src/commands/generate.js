04d31ce816abdd3a7777c6ed1b19807abb7bff25
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-08-12 00:39:25 +0530

    Kaustav | #52 | Implemented generate command for scss file | TG-52

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -1,43 +1,49 @@
-const {Command, flags} = require('@oclif/command')
-const {generateComponent} = require('../helpers/generate.helper')
-const bootstrap = require('../utils/dev-kit/bootstrap')
+const { Command, flags } = require("@oclif/command");
+const {
+  generateComponent,
+  generateStyle
+} = require("../helpers/generate.helper");
+const bootstrap = require("../utils/dev-kit/bootstrap");
 
 class GenerateCommand extends Command {
   async run() {
     const generators = {
       component: generateComponent,
-      c: () => {},
-    }
-    const {args} = this.parse(GenerateCommand)
-    generators[args.generator](args.name, bootstrap)
+      c: generateComponent,
+      style: generateStyle,
+      s: generateStyle
+    };
+    const { args } = this.parse(GenerateCommand);
+    generators[args.generator](args.name, bootstrap);
   }
 }
 
-GenerateCommand.description = 'Generate new files. Such as components, modules etc.'
+GenerateCommand.description =
+  "Generate new files. Such as components, modules etc.";
 
 GenerateCommand.args = [
   {
-    name: 'generator',
+    name: "generator",
     required: true,
-    description: 'What you want to generate',
-    options: ['component'],
+    description: "What you want to generate",
+    options: ["component", "c", "style", "s"]
   },
   {
-    name: 'name',
+    name: "name",
     required: true,
-    description: 'Name of the generated element',
-  },
-]
+    description: "Name of the generated element"
+  }
+];
 
 GenerateCommand.examples = [
-  '$ rg generate component COMPONENT',
-  '$ rg g c COMPONENT',
-]
+  "$ rg generate component COMPONENT",
+  "$ rg g c COMPONENT"
+];
 
-GenerateCommand.aliases = ['g']
+GenerateCommand.aliases = ["g"];
 
 GenerateCommand.flags = {
-  help: flags.help({char: 'h'}),
-}
+  help: flags.help({ char: "h" })
+};
 
-module.exports = GenerateCommand
+module.exports = GenerateCommand;
--- src/helpers/generate.helper.js
+++ src/helpers/generate.helper.js
@@ -1,34 +1,57 @@
-const path = require('path')
-const {Dir, File, Logger} = require('@itchef/rg-lib')
-const {execSync} = require('child_process')
+const path = require("path");
+const { Dir, File, Logger } = require("@itchef/rg-lib");
+const { execSync } = require("child_process");
 
-const updateWithName = (componentPath, name) => {
-  const newComponentPath = componentPath.replace('BasicComponent', name.toLowerCase())
+const updateWithName = (componentPath, name, template) => {
+  const newComponentPath = componentPath.replace(template, name.toLowerCase());
   new File(componentPath)
-  .read()
-  .update('COMPONENT_NAME', name)
-  .write(newComponentPath)
-}
+    .read()
+    .update("COMPONENT_NAME", name)
+    .write(newComponentPath);
+};
 
 const copyBaseFiles = (basicComponentDir, temp) => {
   new Dir(basicComponentDir)
-  .read()
-  .map(file => path.join(basicComponentDir, file))
-  .forEach(filePath => new Dir(filePath).copy(temp))
-}
+    .read()
+    .map(file => path.join(basicComponentDir, file))
+    .forEach(filePath => new Dir(filePath).copy(temp));
+};
 
-const generateComponent = (name, {temp, templates}) => {
-  const workingDirectory = process.cwd()
-  Logger.info(`${name} component is getting created into ${workingDirectory}`)
-  new Dir(temp).cd()
-  let basicComponentDir = path.join(templates._dirName, 'components/basic-component')
-  copyBaseFiles(basicComponentDir, temp)
-  new Dir(temp).read().map(file => updateWithName(file, name))
-  execSync(`rm ${temp}/BasicComponent*`)
-  new Dir(temp).copy(path.join(workingDirectory, name))
-  new Dir(temp).clean()
-}
+const generateComponent = (name, { temp, templates }) => {
+  const workingDirectory = process.cwd();
+  Logger.info(`${name} component is getting created into ${workingDirectory}`);
+  new Dir(temp).cd();
+  let basicComponentDir = path.join(
+    templates._dirName,
+    "components/basic-component"
+  );
+  copyBaseFiles(basicComponentDir, temp);
+  new Dir(temp)
+    .read()
+    .map(file => updateWithName(file, name, "BasicComponent"));
+  execSync(`rm ${temp}/BasicComponent*`);
+  new Dir(temp).copy(path.join(workingDirectory, name));
+  new Dir(temp).clean();
+};
+
+const generateStyle = (name, { temp, templates }) => {
+  const workingDirectory = process.cwd();
+  const tempDir = new Dir(temp);
+  const folder = "styles";
+  const templateInitial = "style";
+  const styles = path.join(templates._dirName, folder);
+
+  Logger.info(`${name} style is getting created into ${workingDirectory}`);
+
+  tempDir.cd();
+  copyBaseFiles(styles, temp);
+  tempDir.read().map(file => updateWithName(file, name, templateInitial));
+  execSync(`rm ${temp}/${templateInitial}*`);
+  new Dir(`${temp}/${name}*`).copy(workingDirectory);
+  new Dir(temp).clean();
+};
 
 module.exports = {
   generateComponent,
-}
+  generateStyle
+};
